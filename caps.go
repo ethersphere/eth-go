1bcef8059ac218144c0043a3dd933df497e82af9
zelig <viktor.tron@gmail.com>
2014-10-18 04:59:44 +0100

    refactor server caps to temp? caps.go

---
+++ caps.go
@@ -0,0 +1,36 @@
+package eth
+
+type Caps byte
+
+const (
+	CapPeerDiscTy Caps = 1 << iota
+	CapTxTy
+	CapChainTy
+
+	CapDefault = CapChainTy | CapTxTy | CapPeerDiscTy
+)
+
+var capsToString = map[Caps]string{
+	CapPeerDiscTy: "Peer discovery",
+	CapTxTy:       "Transaction relaying",
+	CapChainTy:    "Block chain relaying",
+}
+
+func (c Caps) IsCap(cap Caps) bool {
+	return c&cap > 0
+}
+
+func (c Caps) String() string {
+	var caps []string
+	if c.IsCap(CapPeerDiscTy) {
+		caps = append(caps, capsToString[CapPeerDiscTy])
+	}
+	if c.IsCap(CapChainTy) {
+		caps = append(caps, capsToString[CapChainTy])
+	}
+	if c.IsCap(CapTxTy) {
+		caps = append(caps, capsToString[CapTxTy])
+	}
+
+	return strings.Join(caps, " | ")
+}
